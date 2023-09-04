55ef25c899f7cd63753eb32341cabe69658b4ddc
Anand Kumar <anand.kva00@gmail.com>
2023-09-04 18:39:58 +0530

    first commit

--- src/config/db.js
+++ src/config/db.js
@@ -4,10 +4,6 @@ const mongoose = require("mongoose");
 const db = (DB_URI) => {
   mongoose.connect(DB_URI);
 };
-// const db = mongoose.connection;
-// db.on('error', console.error.bind(console, 'Database connection error:'));
-// db.once('open', () => {
-//   console.log('Connected to the database');
-// })
+
 
 module.exports = db;
